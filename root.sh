b5e12ab7014ef4ad3b339d79573cfb86e80893b4
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-19 13:03:06 +0800

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,5 @@
+read -p "自定义ROOT密码:" mima
+echo root:$mima | sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
