aa9640217a0094ee66c0b6271f0d89dd991aa9d7
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-19 13:09:47 +0800

    Create xanmod-kernel.sh

---
+++ xanmod-kernel.sh
@@ -0,0 +1,4 @@
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+apt update && sudo apt install linux-xanmod -y
+rm -rf /etc/iptables && reboot
