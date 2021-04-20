44dacfb45bfba9c195c6febb762b0459acecebbd
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-20 12:31:28 +0800

    Update warp6.sh

--- warp6.sh
+++ warp6.sh
@@ -12,7 +12,5 @@ cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
-grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
 echo -e "\033[1;33m 检测是否成功启动Warp！\n 显示IPV6地址：$(wget -qO- ipv6.ip.sb) \033[0m"
 echo -e "\033[1;32m 如上方显示IPV6地址：2a09.…………，则说明成功啦！\n 如上方无IP显示,（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
-
