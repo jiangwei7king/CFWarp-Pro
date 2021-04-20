8a166c1f0117209191888567d3f95f89fa940eed
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-20 12:17:50 +0800

    Update warp6.sh

--- warp6.sh
+++ warp6.sh
@@ -1,1 +1,20 @@
+echo -e "\033[1;36m 请 注 意！脚 本 仅 支 持 Ubuntu 与 Debain 系 统！\n 针对KVM架构的IPV4 only VPS！！！Warp仅接管IPV6网络！！！ \033[0m"
+apt update && apt install curl sudo lsb-release iptables -y
+echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
+apt update
+apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
+wget -N https://cdn.jsdelivr.net/gh/YG-tsj/EUserv-addv4-warp/wgcf
+cp wgcf /usr/local/bin/wgcf
+chmod +x /usr/local/bin/wgcf
+echo | wgcf register
+wgcf generate
+sed -i 's/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf
+sed -i '/0\.0\.0\.0\/0/d' wgcf-profile.conf
+cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
+cp wgcf-profile.conf /etc/wireguard/wgcf.conf
+systemctl enable wg-quick@wgcf
+systemctl start wg-quick@wgcf
+grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
+echo -e "\033[1;33m 检测是否成功启动Warp！\n 显示IPV6地址：$(wget -qO- ipv6.ip.sb) \033[0m"
+echo -e "\033[1;32m 如上方显示IPV6地址：2a09.…………，则说明成功啦！\n 如上方无IP显示,（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
 
