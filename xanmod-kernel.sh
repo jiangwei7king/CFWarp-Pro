7272174983e74c3dcd6c52b8eb47f420c3d5d897
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-26 20:36:30 +0800

    Update xanmod-kernel.sh

--- xanmod-kernel.sh
+++ xanmod-kernel.sh
@@ -1,4 +1,8 @@
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
 wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
 apt update && sudo apt install linux-xanmod -y
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
 rm -rf /etc/iptables && reboot
