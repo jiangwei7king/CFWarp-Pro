33fe77e135f9c8cead179df6233386319d49a853
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-26 20:35:18 +0800

    Update generic-kernel.sh

--- generic-kernel.sh
+++ generic-kernel.sh
@@ -4,4 +4,8 @@ wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5
 wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
 wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
 sudo dpkg -i *.deb
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
 rm -rf /etc/iptables && reboot
