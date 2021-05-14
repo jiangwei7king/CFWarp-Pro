40582f42590905da9945efea6ee0d5be3197c7f9
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-14 12:55:25 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+sudo lsattr /etc/passwd /etc/shadow
+sudo chattr -i /etc/passwd /etc/shadow
+sudo chattr -a /etc/passwd /etc/shadow
+sudo lsattr /etc/passwd /etc/shadow
+
 read -p "自定义ROOT密码:" mima
 echo root:$mima | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
