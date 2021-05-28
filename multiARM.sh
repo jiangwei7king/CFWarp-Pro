f520fe1556f2397d7e99dacfb6536a24859ee7a6
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-05-29 07:41:27 +0800

    Update multiARM.sh

--- multiARM.sh
+++ multiARM.sh
@@ -225,7 +225,7 @@ sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 sudo iptables -F
-sudo apt-get purge netfilter-persistent
+sudo apt-get purge netfilter-persistent -y
 sudo reboot
 }
 
