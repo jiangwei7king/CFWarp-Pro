b95a8d086d5fa4a91c20e7765b10694d0f579108
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-12 14:11:24 +0800

    Update ccore.sh

--- ccore.sh
+++ ccore.sh
@@ -35,7 +35,7 @@ reboot
 function ub(){
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 chmod +x ubuntu-mainline-kernel.sh
-sudo mv ubuntu-mainline-kernel.sh /tmp
+sudo mv ubuntu-mainline-kernel.sh /usr/local/bin/
 sudo ubuntu-mainline-kernel.sh -i -y
 reboot
 }
