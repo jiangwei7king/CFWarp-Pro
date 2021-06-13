e074e1965d55b8cc774b8251f5fd27a8f5acbd10
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-13 10:19:58 +0800

    Update and rename ccore.sh to upcore.sh

--- upcore.sh
+++ upcore.sh
@@ -33,11 +33,12 @@ reboot
 }
 
 function ub(){
-wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
-chmod +x ubuntu-mainline-kernel.sh
-sudo mv ubuntu-mainline-kernel.sh /usr/local/bin/
-sudo ubuntu-mainline-kernel.sh -i
-sudo apt -f install -y
+cd /tmp
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb 
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+sudo dpkg -i *.deb
 reboot
 }
 
