3804979b72c48c04b64bf76efdfa291f85f41f48
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-19 06:47:28 +0800

    Update upcore.sh

--- upcore.sh
+++ upcore.sh
@@ -33,6 +33,8 @@ reboot
 }
 
 function ub(){
+
+function v46(){
 cd /tmp
 wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
 wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb 
@@ -40,6 +42,41 @@ wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5
 wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
 sudo dpkg -i *.deb
 reboot
+}
+
+function v6(){
+echo -e nameserver 2a00:1098:2c::1 > /etc/resolv.conf
+cd /tmp
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb 
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+wget --no-check-certificate -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+sudo dpkg -i *.deb
+reboot
+}
+
+function menu(){
+    clear
+    green " 请确认当前的VPS属于以下哪种IP表现形式！" 
+    blue " 1. 纯IPV4/双栈IPV4+IPV6 "    
+    blue " 2. 纯IPV6 "
+    red " 0. 返回上一层 "
+    echo
+    read -p "请输入数字:" menuNumberInput
+    case "$menuNumberInput" in   
+     1 )
+        v46
+     ;;
+     2 )
+        v6
+     ;;
+     0 )
+       start_menu
+     ;;
+      esac
+}
+
+menu  
 }
 
 function de(){
